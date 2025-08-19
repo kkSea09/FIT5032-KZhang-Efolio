e27b8483717bcf676b0c70ee4cd9575f3926ad43
kkSea09 <kzha0110@student.monash.edu>
2025-08-19 16:18:49 +1000

    Completed Activity 1-7

--- kzhang-library/src/App.vue
+++ kzhang-library/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import JSON from './components/JSON.vue';
+import Form from './components/Form.vue';
+
 </script>
 
 <template>
   <p>
-    <JSON />
+    <!-- <JSON /> -->
+    <Form />
   </p>
 </template>
 
 <style scoped></style>
-  
