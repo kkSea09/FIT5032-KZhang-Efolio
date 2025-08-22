d082e53da5ccf202514a7d16d956451c767f33b5
kkSea09 <kzha0110@student.monash.edu>
2025-08-22 19:14:51 +1000

    Complete eFolio - Week 4 activity (1-4)

--- kzhang-library/src/main.js
+++ kzhang-library/src/main.js
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import { Theme } from '@primevue/themes';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 createApp(App).mount('#app')
