1088ba7828c155efac519761f63e99e60977c968
kkSea09 <kzha0110@student.monash.edu>
2025-10-23 18:17:53 +1100

    Modified vite.config.js

--- kzhang-library/vite.config.js
+++ kzhang-library/vite.config.js
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032-KZhang-Efolio/'
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),
